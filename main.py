edb90b02a86139e274bc1fdd80767373b1544f59
Pavlo <71569680+p4v10@users.noreply.github.com>
2021-01-08 13:24:32 -0600

    added text

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ st.set_page_config(page_title='UFC')
 st.write("""
 ### Predicting who win the fight
 """)
+
+st.write("""
+In this project we are going to use `Random Forest` to predict the winner of the fight.
+""")
+
+st.write("""
+This dataset is imported [Kaggle](https://www.kaggle.com/rajeevw/ufcdata). Cointains a list of every UFC fight from 1993 to 2019.
+""")
+
+
+st.image('img/img2.png', width=650, height=130)
+data =
+
+st.multiselect('Chose the charts that you are interested in', options=['dasda','asdasd','dasdasd','adsad','dasdas','dasdasd'])
