fbb7108815eb4187e582b6ad2c657aa74e79ad82
Pavlo <71569680+p4v10@users.noreply.github.com>
2021-01-08 12:33:17 -0600

    first

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+st.set_page_config(page_title='UFC')
+
+st.write("""
+### Predicting who win the fight
+""")
