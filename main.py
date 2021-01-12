2ea173535451ec9dae2cb0311a7be2f22c5ea1ba
Pavlo <71569680+p4v10@users.noreply.github.com>
2021-01-12 13:04:37 -0600

    charts

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ chart_p = st.multiselect('Chose the charts that you are interested in', options=
 
 df = pd.read_csv('data/new_data.csv')
 
-
+# Ufc events per year
 values = df['date_year'].sort_values(ascending=False).value_counts().sort_index()
 labels = values.index
 
@@ -47,7 +47,7 @@ plt.ylabel('Quantity of fights')
 plt.title('UFC Events Per Year')
 st.pyplot(fig1)
 
-
+#Events by country
 fig2 = plt.figure(figsize=(16,8))
 bar = sns.countplot(df['country'], palette='magma')
 plt.xticks(rotation=90)
@@ -62,7 +62,41 @@ plt.title('Event by Country')
 st.pyplot(fig2)
 
 
+#Divisions
+fig4 = plt.figure(figsize=(16,8))
+sns.countplot(y=df['weight_class'],palette='magma')
+sns.set(style='white')
+plt.title('Fighters Divisions')
+st.pyplot(fig4)
 
+
+#by fight win
+values = df['win_by'].value_counts()
+labels = values.index
+fig5 = plt.figure(figsize=(16,8))
+plt.title('UFC Fight Win By')
+sns.barplot(x=values,y=labels,palette='magma')
+st.pyplot(fig5)
+
+#outcome by division
+bar = df.groupby(['weight_class', 'win_by']).size().reset_index().pivot(columns='win_by', index='weight_class', values=0)
+bar.plot(kind='barh', stacked=True, figsize=(16,8))
+plt.legend(bbox_to_anchor=(1.23, 0.99), loc=1, borderaxespad=0.)
+plt.title('UFC Fight Outcome by Division')
+plt.ylabel('Division')
+st.pyplot()
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+
+
+
+
+
+
+
+
+#men & women fights
 women = df.weight_class.str.contains('Women')
 women1 = len(df[women])
 men = (len(df['weight_class'])) - len(df[women])
@@ -72,6 +106,6 @@ sizes = [men, women1]
 explode = (0, 0.1)
 
 fig3 = fig3, ax3 = plt.subplots(figsize=(16,8))
-ax3.pie(sizes, explode=explode, labels=labels,autopct='%1.1f%%', shadow=True, startangle=90)
+ax3.pie(sizes, explode=explode, labels=labels,autopct='%1.1f%%', shadow=False, startangle=45)
 ax3.axis('equal')
 st.pyplot(fig3)
