dff1b003fde9597e394c5705cc7698f996b2bbf7
Pavlo <71569680+p4v10@users.noreply.github.com>
2021-01-12 12:28:46 -0600

    charts

--- main.py
+++ main.py
@@ -44,5 +44,34 @@ for p in ax.patches:
            fontsize=12, color='black', ha='center', va='bottom')
 plt.xlabel('Year')
 plt.ylabel('Quantity of fights')
-plt.title('UFC Event Per Year')
+plt.title('UFC Events Per Year')
 st.pyplot(fig1)
+
+
+fig2 = plt.figure(figsize=(16,8))
+bar = sns.countplot(df['country'], palette='magma')
+plt.xticks(rotation=90)
+ax = plt.gca()
+y_max = df['country'].value_counts().max()
+ax.set_ylim(1)
+for p in ax.patches:
+    ax.text(p.get_x() + p.get_width()/2., p.get_height(), p.get_height(),
+           fontsize=10, color='black', ha='center', va='bottom')
+
+plt.title('Event by Country')
+st.pyplot(fig2)
+
+
+
+women = df.weight_class.str.contains('Women')
+women1 = len(df[women])
+men = (len(df['weight_class'])) - len(df[women])
+
+labels = 'Men Fights', 'Women Fights'
+sizes = [men, women1]
+explode = (0, 0.1)
+
+fig3 = fig3, ax3 = plt.subplots(figsize=(16,8))
+ax3.pie(sizes, explode=explode, labels=labels,autopct='%1.1f%%', shadow=True, startangle=90)
+ax3.axis('equal')
+st.pyplot(fig3)
