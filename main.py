27fb7ab2cb6651f61e91bde25c18b2982c3d0242
Pavlo <71569680+p4v10@users.noreply.github.com>
2021-01-22 16:08:46 -0600

    select

--- main.py
+++ main.py
@@ -19,15 +19,28 @@ st.write("""
 This dataset is imported from [Kaggle](https://www.kaggle.com/rajeevw/ufcdata). Cointains a list of every UFC fight from 1993 to 2019.
 """)
 
-
-st.image('img/img2.png', width=695)
-
-chart_p = st.multiselect('Chose the charts that you are interested in', options=['Events per Year','asdasd','dasdasd','adsad','dasdas','dasdasd'])
-
-
 df = pd.read_csv('data/new_data.csv')
+st.image('img/img2.png', width=695)
+choices = {1: 'Events per Year',
+2: 'Events by Country',
+3: 'Fighters Divisions',
+4: 'Fight Win By',
+5: 'Result by Division',
+6: 'Men vs Women Distribution '}
+
+def format_func(option):
+    return choices[option]
+
+
+
+option = st.multiselect('Chose the charts that you are interested in', options=list(choices.keys()), format_func=format_func)
+
+
+
+
 
 # Ufc events per year
+
 values = df['date_year'].sort_values(ascending=False).value_counts().sort_index()
 labels = values.index
 
@@ -45,7 +58,15 @@ for p in ax.patches:
 plt.xlabel('Year')
 plt.ylabel('Quantity of fights')
 plt.title('UFC Events Per Year')
-st.pyplot(fig1)
+
+
+
+
+
+
+
+
+
 
 #Events by country
 fig2 = plt.figure(figsize=(16,8))
@@ -59,41 +80,32 @@ for p in ax.patches:
            fontsize=10, color='black', ha='center', va='bottom')
 
 plt.title('Event by Country')
-st.pyplot(fig2)
+
 
 
 #Divisions
-fig4 = plt.figure(figsize=(16,8))
+fig3 = plt.figure(figsize=(16,8))
 sns.countplot(y=df['weight_class'],palette='magma')
 sns.set(style='white')
 plt.title('Fighters Divisions')
-st.pyplot(fig4)
+
 
 
 #by fight win
 values = df['win_by'].value_counts()
 labels = values.index
-fig5 = plt.figure(figsize=(16,8))
+fig4 = plt.figure(figsize=(16,8))
 plt.title('UFC Fight Win By')
 sns.barplot(x=values,y=labels,palette='magma')
-st.pyplot(fig5)
+
 
 #outcome by division
 bar = df.groupby(['weight_class', 'win_by']).size().reset_index().pivot(columns='win_by', index='weight_class', values=0)
-bar.plot(kind='barh', stacked=True, figsize=(16,8))
+fig5 = bar.plot(kind='barh', stacked=True, figsize=(16,8))
 plt.legend(bbox_to_anchor=(1.23, 0.99), loc=1, borderaxespad=0.)
 plt.title('UFC Fight Outcome by Division')
 plt.ylabel('Division')
-st.pyplot()
 st.set_option('deprecation.showPyplotGlobalUse', False)
-
-
-
-
-
-
-
-
 
 
 #men & women fights
@@ -105,7 +117,27 @@ labels = 'Men Fights', 'Women Fights'
 sizes = [men, women1]
 explode = (0, 0.1)
 
-fig3 = fig3, ax3 = plt.subplots(figsize=(16,8))
+fig6 = fig6, ax3 = plt.subplots(figsize=(16,8))
 ax3.pie(sizes, explode=explode, labels=labels,autopct='%1.1f%%', shadow=False, startangle=45)
+plt.title('Men vs Women')
 ax3.axis('equal')
-st.pyplot(fig3)
+
+
+
+st.write(option)
+def back_multi(option):
+    for x in option:
+        if x == 1:
+            st.pyplot(fig1)
+        elif x == 2:
+            st.pyplot(fig2)
+        elif x == 3:
+            st.pyplot(fig3)
+        elif x == 4:
+            st.pyplot(fig4)
+        elif x == 5:
+            st.pyplot(fig5)
+        elif x == 6:
+            st.pyplot(fig6)
+
+back_multi(option)
