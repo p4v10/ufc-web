d3d4784bd21ec883146701c4c32897b7920115e2
Pavlo <71569680+p4v10@users.noreply.github.com>
2021-01-27 13:26:44 -0600

    modeling

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pickle
+import re
+from sklearn.pipeline import Pipeline
 
 
 st.set_page_config(page_title='UFC')
@@ -114,3 +117,50 @@ def back_multi(option):
             st.pyplot(fig6)
 
 back_multi(option)
+
+
+filename_rf = 'model/ufc_model.sav'
+rf_model = pickle.load(open(filename_rf, 'rb'))
+pred_df = pd.read_csv('data/data.csv')
+
+
+def predict(pred_df, pipeline, blue_fighter, red_fighter, weightclass, rounds, title_bout=False):
+
+    #We build two dataframes, one for each figther
+    f1 = pred_df[(pred_df['R_fighter'] == blue_fighter) | (pred_df['B_fighter'] == blue_fighter)].copy()
+    f1.reset_index(drop=True, inplace=True)
+    f1 = f1[:1]
+    f2 = pred_df[(pred_df['R_fighter'] == red_fighter) | (pred_df['B_fighter'] == red_fighter)].copy()
+    f2.reset_index(drop=True, inplace=True)
+    f2 = f2[:1]
+
+    # if the fighter was red/blue corner on his last fight, we filter columns to only keep his statistics (and not the other fighter)
+    # then we rename columns according to the color of  the corner in the parameters using re.sub()
+    if (f1.loc[0, ['R_fighter']].values[0]) == blue_fighter:
+        result1 = f1.filter(regex='^R', axis=1).copy() #here we keep the red corner stats
+        result1.rename(columns = lambda x: re.sub('^R','B', x), inplace=True)  #we rename it with "B_" prefix because he's in the blue_corner
+    else:
+        result1 = f1.filter(regex='^B', axis=1).copy()
+    if (f2.loc[0, ['R_fighter']].values[0]) == red_fighter:
+        result2 = f2.filter(regex='^R', axis=1).copy()
+    else:
+        result2 = f2.filter(regex='^B', axis=1).copy()
+        result2.rename(columns = lambda x: re.sub('^B','R', x), inplace=True)
+
+    fight = pd.concat([result1, result2], axis = 1) # we concatenate the red and blue fighter dataframes (in columns)
+    fight.drop(['R_fighter','B_fighter'], axis = 1, inplace = True) # we remove fighter names
+    fight.insert(0, 'title_bout', title_bout) # we add tittle_bout, weight class and number of rounds data to the dataframe
+    fight.insert(1, 'weight_class', weightclass)
+    fight.insert(2, 'no_of_rounds', rounds)
+    fight['title_bout'] = fight['title_bout'].replace({True: 1, False: 0})
+
+    pred = pipeline.predict(fight)
+    proba = pipeline.predict_proba(fight)
+    if (pred == 1.0):
+        print("The predicted winner is", red_fighter, 'with a probability of', round(proba[0][1] * 100, 2), "%")
+    else:
+        print("The predicted winner is", blue_fighter, 'with a probability of ', round(proba[0][0] * 100, 2), "%")
+    return proba
+
+
+predict(pred_df,rf_model, 'Raphael Assuncao', 'Cody Garbrandt', 'Bantamweight', 3, False)
