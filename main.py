b53077bd08a7cc5c7962f9a283ff077e1ce6f5a9
Pavlo <71569680+p4v10@users.noreply.github.com>
2021-03-15 22:11:28 -0500

    finished

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pickle
-import re
-from sklearn.pipeline import Pipeline
+
 
 
 st.set_page_config(page_title='UFC')
